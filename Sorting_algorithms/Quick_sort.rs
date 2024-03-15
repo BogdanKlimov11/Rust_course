d9676cc149d4426b21c602a50b0467e5eab12bd3
Bogdan Klimov <136115919+BogdanKlimov11@users.noreply.github.com>
2024-03-15 19:58:33 +0300

    Create Quick_sort.rs

---
+++ Sorting_algorithms/Quick_sort.rs
@@ -0,0 +1,29 @@
+fn quick_sort(arr: &mut [i32]) {
+    if arr.len() <= 1 {
+        return;
+    }
+    let pivot = arr[arr.len() / 2];
+    let mut left = 0;
+    let mut right = arr.len() - 1;
+
+    while left <= right {
+        while arr[left] < pivot {
+            left += 1;
+        }
+        while arr[right] > pivot {
+            right -= 1;
+        }
+        if left <= right {
+            arr.swap(left, right);
+            left += 1;
+            right -= 1;
+        }
+    }
+
+    if right > 0 {
+        quicksort(&mut arr[..=right]);
+    }
+    if left < arr.len() {
+        quicksort(&mut arr[left..]);
+    }
+}
